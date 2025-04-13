0e694a8e5707b55dd9467faf47db1ba4603f5805
Hetav21 <shahhetav2106@gmail.com>
2025-04-13 20:58:48 +0530

    ..

--- server/dist/common/schema/redirectSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redirectSchema = void 0;
-const zod_1 = require("zod");
-const createLinkSchema_1 = require("./createLinkSchema");
-exports.redirectSchema = zod_1.z.object({
-    shortUrl: createLinkSchema_1.customAliasSchema,
-});
-//# sourceMappingURL=redirectSchema.js.map
